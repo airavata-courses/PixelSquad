c97b92d0c6b78d300f7184c431a55f198dac67e6
deepika awasthi <danagar0312@gmail.com>
2021-02-28 22:42:44 -0500

    api gateway | UI | photometadatasearchservice

--- mainuploadmgmt/src/main/java/com/iu/photosquad/mainuploadmgmt/mainuploadmgmt/Repo/UserRepo.java
+++ mainuploadmgmt/src/main/java/com/iu/photosquad/mainuploadmgmt/mainuploadmgmt/Repo/UserRepo.java
@@ -2,9 +2,11 @@ package com.iu.photosquad.mainuploadmgmt.mainuploadmgmt.Repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.iu.photosquad.mainuploadmgmt.mainuploadmgmt.Entity.Album;
 import com.iu.photosquad.mainuploadmgmt.mainuploadmgmt.Entity.User;
 
 public interface UserRepo extends JpaRepository<User,Long>{
 	User findByUsername(String username);
+	
+//	@Query("SELECT new com.iu.photosquad.mainuploadmgmt.mainuploadmgmt.Dto(a.albumname, u.username) FROM user u JOIN  u.albums a")
+//	List<TotalAlbumsPerUser> getUserAlbumInformation();
 }
